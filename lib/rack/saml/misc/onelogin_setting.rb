b5c334dac2b79355fcc26eb708d6e18ee7bc877b
Chris Kerlin <chris.kerlin@gmail.com>
2016-04-21 14:18:38 -0400

    Update onelogin_setting.rb
    Add settings for certificate (to be included in metadata), and private key (for decrypting encrypted assertions.)

--- lib/rack/saml/misc/onelogin_setting.rb
+++ lib/rack/saml/misc/onelogin_setting.rb
@@ -7,6 +7,8 @@ module Rack
         settings = OneLogin::RubySaml::Settings.new
         settings.assertion_consumer_service_url = @config['assertion_consumer_service_uri']
         settings.issuer = @config['saml_sp']
+        settings.certificate = IO::File.open(@config['certificate_path'], "r").read if @config['certificate_path']
+        settings.private_key = IO::File.open(@config['private_key_path'], "r").read if @config['private_key_path']
         settings.idp_sso_target_url = @metadata['saml2_http_redirect']
         settings.idp_cert = @metadata['certificate']
         settings.name_identifier_format = "urn:oasis:names:tc:SAML:2.0:nameid-format:transient"
